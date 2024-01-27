5eb59bc8031c5c32f4f51d85fde61205cfe9dc2a
NickyLarcin <48786497+NickyLarcin@users.noreply.github.com>
2024-01-27 18:03:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,25 @@ const lifeVariablesContainer = document.getElementById(
 
 const submitButton = document.getElementById("submit-button");
 const downloadButton = document.getElementById("download-button");
+const hiddenMenuButton = document.getElementById("hidden-menu-button");
+const calendarSubcontainer = document.getElementById("calendar-subcontainer");
 
 const dataOutput = {};
 
+let resultsLinkedToDate =
+    JSON.parse(localStorage.getItem("dataRecorderData")) === null
+      ? {}
+      : JSON.parse(localStorage.getItem("dataRecorderData"));
 const lifeVariables = [
+  {
+    dataText: "Bed Time",
+    description: "When you went to bed",
+    dataType: "time",
+    dataTypeOptions: "",
+    defaultValue: "23:00",
+    optNumMinMax: "",
+    inputHelp: "Select the appropriate time"
+  },
   {
     dataText: "Mood",
     description: "How you've felt during the day",
@@ -30,6 +45,24 @@ const lifeVariables = [
     defaultValue: 0,
     optNumMinMax: [0, 3],
     inputHelp: "Drag to the appropriate feeling"
+  },
+  {
+    dataText: "Reading",
+    description: "How long you read today",
+    dataType: "minutes",
+    dataTypeOptions: "",
+    defaultValue: "0",
+    optNumMinMax: "",
+    inputHelp: "Estimates in minutes"
+  },
+  {
+    dataText: "Side Project",
+    description: "How long you did work towards freedom today",
+    dataType: "minutes",
+    dataTypeOptions: "",
+    defaultValue: "0",
+    optNumMinMax: "",
+    inputHelp: "Estimates in minutes"
   },
   {
     dataText: "Journal",
@@ -68,15 +101,18 @@ const displayVariables = () => {
       <hr class="life-variable-separator"/>
       
       <div class="life-variable-select">
+      <i class="gg-close"></i>
         <label for="${dataText.toLowerCase()}-input" class="input-label">${inputHelp}</label>
         <div class="range-container">
+        
           <input type="range" id="${dataText.toLowerCase()}-input" name="${dataText.toLowerCase()}-input" min="${
           optNumMinMax[0]
         }" max="${
           optNumMinMax[1]
         }" value="${defaultValue}" oninput="updateValue(value, '${dataText.toLowerCase()}input-value')">
           <span id="${dataText.toLowerCase()}input-value">${defaultValue}</span>
-          </div>
+          </div
+          
       </div>
     </div>
      `;
@@ -91,6 +127,7 @@ const displayVariables = () => {
       </div>
       <hr class="life-variable-separator"/>
       <div class="life-variable-select">
+      <i class="gg-close"></i>
    
           <textarea id="${dataText.toLowerCase()}-input" rows="4" cols="50" class="input-text">${inputHelp}
         </textarea>
@@ -104,7 +141,6 @@ const displayVariables = () => {
         break;
 
       case "textRange":
-        console.log(lifeVariables[index]["dataTypeOptions"]);
         lifeVariablesContainer.innerHTML += `
           <div class="life-variable addShadow">
       <div class="life-variable-info">
@@ -113,6 +149,7 @@ const displayVariables = () => {
       </div>
       <hr class="life-variable-separator"/>
       <div class="life-variable-select">
+      <i class="gg-close"></i>
         <label for="${dataText.toLowerCase()}-input" class="input-label">${inputHelp}</label>
         <div class="range-container">
           <input type="range" id="${dataText.toLowerCase()}-input" name="${dataText.toLowerCase()}-input" min="${
@@ -125,6 +162,56 @@ const displayVariables = () => {
       </div>
     </div>
      `;
+        break;
+
+      case "time":
+        lifeVariablesContainer.innerHTML += `
+        <div class="life-variable addShadow">
+      <div class="life-variable-info">
+        <p class="variable-title">${dataText}</p>
+        <p class="variable-description">${description}</p>
+      </div>
+      
+      <hr class="life-variable-separator"/>
+      
+      <div class="life-variable-select">
+        <i class="gg-close"></i>
+        <label for="${dataText.toLowerCase()}-input" class="input-label">${inputHelp}</label>
+        <input type="time" id="${dataText.toLowerCase()}-input" class="input-time" value="${defaultValue}">
+      
+          
+      </div>
+    </div>
+    
+     `;
+        break;
+
+      case "minutes":
+        lifeVariablesContainer.innerHTML += `
+        
+        <div class="life-variable addShadow">
+      <div class="life-variable-info">
+        <p class="variable-title">${dataText}</p>
+        <p class="variable-description">${description}</p>
+      </div>
+      
+      <hr class="life-variable-separator"/>
+      
+      <div class="life-variable-select">
+        <i class="gg-close"></i>
+        <label for="${dataText.toLowerCase()}-input" class="input-label">${inputHelp}</label>
+        <input type="number" id="${dataText.toLowerCase()}-input" class="input-number" value="${defaultValue}">
+      
+          
+      </div>
+    </div>
+        
+        
+        
+        
+        
+        
+        `;
         break;
     }
   });
@@ -176,6 +263,17 @@ function getResults() {
         ).value;
 
         break;
+      case "time":
+        resultList[`${dataText.toLowerCase()}`] = document.getElementById(
+          `${dataText.toLowerCase()}-input`
+        ).value;
+        break;
+
+      case "minutes":
+        resultList[`${dataText.toLowerCase()}`] = document.getElementById(
+          `${dataText.toLowerCase()}-input`
+        ).value;
+        break;
     }
   });
 
@@ -183,16 +281,19 @@ function getResults() {
 }
 
 function submitForm() {
+  // to fetch date ; fetch existing Data in LocalStorage ; add new entry based on date to the Data ; Store data back to LocalStorage
+
+  const date = document.getElementById("input-record-date").value;
+
+  if (!date) {
+    alert("Select a Date !");
+    return;
+  }
+
   console.log("test1");
   console.log(JSON.parse(localStorage.getItem("dataRecorderData")));
 
-  resultsLinkedToDate =
-    JSON.parse(localStorage.getItem("dataRecorderData")) === null
-      ? {}
-      : JSON.parse(localStorage.getItem("dataRecorderData"));
-
-  //const resultsLinkedToDate = {}
-  const date = document.getElementById("input-record-date").value;
+  
 
   console.log("test2");
   console.log(resultsLinkedToDate);
@@ -204,6 +305,8 @@ function submitForm() {
   localStorage.setItem("dataRecorderData", JSON.stringify(resultsLinkedToDate));
   return resultsLinkedToDate;
 }
+
+// EXCEL Export
 
 const reshapeResultsToExportFormat = () => {
   const convertedArray = Object.keys(submitForm()).map((dateKey) => {
@@ -217,7 +320,8 @@ const reshapeResultsToExportFormat = () => {
 };
 
 submitButton.addEventListener("click", () => {
-  submitForm();
+  submitForm()
+  displayCalendar(01,2024);
 });
 
 downloadButton.addEventListener("click", () => {
@@ -239,3 +343,76 @@ function exportResultsToExcel(results) {
   // Download
   XLSX.writeFile(workbook, "Results.xlsx");
 }
+
+/// CALENDAR
+
+ 
+  // Prints a month(P1) of a given year (P2) in the calendarSubcontainer; each day has an Id with the format "yyyy-mm-dd"
+  const displayCalendar = (month, year) => {
+    const printWeekDaysHeader = () => {
+      for (let i = 0; i < 7; i++) {
+        calendarSubcontainer.innerHTML += `<div class="week-day-header">${
+          ["M", "T", "W", "T", "F", "S", "S"][i]
+        }</div>`;
+      }
+    };
+
+    const printNumberDays = () => {
+      for (let i = 1; i < daysInMonth + firstDayOfMonth; i++) {
+        const day = i - firstDayOfMonth + 1;
+
+        calendarSubcontainer.innerHTML += `<div id="day-id-${year}-${month}-${day}" class="day-number">${
+          i - firstDayOfMonth + 1 >= 0 ? day : 0
+        }</div>`;
+        
+        
+        
+        
+        if(resultsLinkedToDate[formatDate(year,month,day)]!== undefined){
+          document.getElementById(`day-id-${year}-${month}-${day}`).classList.add("recorded-date")
+        } 
+      }
+    };
+
+    const numberOfCellsInGrid = 42; //6*7
+    const daysInMonth = getDaysInMonth(year, month);
+    const firstDayOfMonth = getFirstDayOfMonth(year, month);
+
+    printWeekDaysHeader();
+    printNumberDays();
+    
+  }
+  
+  
+
+
+displayCalendar(01,2024)
+
+
+
+// UTILITY FUNCTIONS
+
+function getDaysInMonth(year, month) {
+  // The month argument in JavaScript is zero-based, so we subtract 1 from the input month.
+  const date = new Date(year, month - 1, 1);
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(date.getDate() - 1);
+  return date.getDate();
+}
+
+function getFirstDayOfMonth(year, month) {
+  // The month argument in JavaScript is zero-based, so we subtract 1 from the input month.
+  return new Date(year, month - 1, 1).getDay();
+}
+
+function formatDate(year, month, day) {
+  // Ensure month and day are two digits
+  month = String(month).padStart(2, '0');
+  day = String(day).padStart(2, '0');
+
+  // Combine the parts into the "YYYY-MM-DD" format
+  const formattedDate = `${year}-${month}-${day}`;
+  
+  return formattedDate;
+}
+
